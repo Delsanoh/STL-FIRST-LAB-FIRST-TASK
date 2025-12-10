28092d74df897f330e61ac39d564e46671a4c4b0
Delsanoh <164777062+Delsanoh@users.noreply.github.com>
2025-12-10 15:40:16 +0300

    LABA FULL

--- Первая стл лаба первое задание/functions.h
+++ Первая стл лаба первое задание/functions.h
@@ -6,12 +6,12 @@
 #include <limits>
 #include <iostream>
 
-void fillMatrix(std::vector<std::vector<int>>&, int, int);
-void printMatrix(const std::vector<std::vector<int>>& );
-void findMaxElement(const std::vector<std::vector<int>>&, int&, int&);
+void fillMatrix(std::vector<std::vector<int32_t>>&, int32_t, int32_t);
+void printMatrix(const std::vector<std::vector<int32_t>>& );
+void findMaxElement(const std::vector<std::vector<int32_t>>&, int32_t&, int32_t&);
 
-void deleteRowAndColumn(std::vector<std::vector<int>>& , int, int);
-int calculateRowSum(const std::vector<int>& );
-void sortRowsBySum(std::vector<std::vector<int>>&);
+void deleteRowAndColumn(std::vector<std::vector<int32_t>>& , int32_t, int32_t);
+int calculateRowSum(const std::vector<int32_t>& );
+void sortRowsBySum(std::vector<std::vector<int32_t>>&);
 
 #endif
