0f700c40b6a15e1fd7a30b3f4e220e975f2a9c19
Delsanoh <164777062+Delsanoh@users.noreply.github.com>
2025-12-13 14:36:42 +0300

    ВАУ

--- Первая стл лаба первое задание/functions.cpp
+++ Первая стл лаба первое задание/functions.cpp
@@ -5,9 +5,7 @@ void fillMatrix(std::vector<std::vector<int32_t>>& matrix, int32_t minVal, int32
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dist(minVal, maxVal);
 
-    // rowIt вычисляется один раз в начале внешнего цикла, rowEnd - один раз
     for (std::vector<std::vector<int32_t>>::iterator rowIt = matrix.begin(); rowIt != matrix.end(); ++rowIt) {
-        // Выносим colEnd, так как rowIt->end() вычисляется многократно
         std::vector<int32_t>::iterator colIt = rowIt->begin();
         std::vector<int32_t>::iterator colEnd = rowIt->end();
 
@@ -18,11 +16,9 @@ void fillMatrix(std::vector<std::vector<int32_t>>& matrix, int32_t minVal, int32
 }
 
 void printMatrix(const std::vector<std::vector<int>>& matrix) {
-    // rowEnd вычисляется один раз
     std::vector<std::vector<int32_t>>::const_iterator rowEnd = matrix.end();
 
     for (std::vector<std::vector<int32_t>>::const_iterator rowIt = matrix.begin(); rowIt != rowEnd; ++rowIt) {
-        // Выносим colEnd, так как rowIt->end() вычисляется многократно
         std::vector<int32_t>::const_iterator colIt = rowIt->begin();
         std::vector<int32_t>::const_iterator colEnd = rowIt->end();
 
@@ -39,12 +35,10 @@ void findMaxElement(const std::vector<std::vector<int32_t>>& matrix, int32_t& ma
     maxCol = -1;
 
     int32_t rowIndex = 0;
-    // rowEnd вычисляется один раз
     std::vector<std::vector<int32_t>>::const_iterator rowEnd = matrix.end();
 
     for (std::vector<std::vector<int32_t>>::const_iterator rowIt = matrix.begin(); rowIt != rowEnd; ++rowIt, ++rowIndex) {
         int colIndex = 0;
-        // colEnd вычисляется один раз для каждой строки
         std::vector<int32_t>::const_iterator colIt = rowIt->begin();
         std::vector<int32_t>::const_iterator colEnd = rowIt->end();
 
@@ -66,7 +60,6 @@ void deleteRowAndColumn(std::vector<std::vector<int32_t>>& matrix, int32_t rowTo
     }
 
     if (colToDelete >= 0) {
-        // rowEnd вычисляется один раз
         std::vector<std::vector<int32_t>>::iterator rowEnd = matrix.end();
 
         for (std::vector<std::vector<int32_t>>::iterator rowIt = matrix.begin(); rowIt != rowEnd; ++rowIt) {
